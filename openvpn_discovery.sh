721953bf5b915d9b3ecf4d754af0527fa0e68f17
El de Rone <elderone@users.noreply.github.com>
2016-05-25 13:09:55 +0300

    Update openvpn_discovery.sh

--- openvpn_discovery.sh
+++ openvpn_discovery.sh
@@ -1,10 +1,17 @@
-#/bin/sh
+#!/bin/bash
 
-OPENVPN_CONF_DIR=$(echo $2 | grep -o "^/[^\s]+" || echo "/var/etc/openvpn/")
+OPENVPN_CONF_DIR=$(echo $2 | grep -o "^/[^\s]+" || echo "/etc/openvpn")
 
-for i in `find ${OPENVPN_CONF_DIR} -iname "$1*.conf" -exec grep management {} \; | awk '{print $2}'`
-do
-	instances="$instances,"'{"{#'$1'}":"'$i'","{#'$1'_NAME}":"'`basename $i .sock`'"}'
+for conf in ${OPENVPN_CONF_DIR}/*.conf; do
+    if [[ $1 = CLIENT && $(grep -c ^remote ${conf}) > 0 ]]; then
+	socket=$(grep management ${conf} | cut -d' ' -f2)
+	name=$(basename ${conf} .conf)
+	instances="$instances,"'{"{#'$1'}":"'${socket}'","{#'$1'_NAME}":"'${name}'"}'
+    elif [[ $1 = SERVER && $(grep -c ^remote ${conf}) = 0 ]]; then
+	socket=$(grep management ${conf} | cut -d' ' -f2)
+	name=$(basename ${conf} .conf)
+	instances="$instances,"'{"{#'$1'}":"'${socket}'","{#'$1'_NAME}":"'${name}'"}'
+    fi
 done
 
 echo '{"data":['${instances#,}']}'
